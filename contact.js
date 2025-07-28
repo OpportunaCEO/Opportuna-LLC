a22d66d9fe852de8040e0f711664c3978cd1fbe1
OpportunaCEO <luke.dodson04@gmail.com>
2025-07-28 11:52:15 -0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,8 @@
+const form = document.getElementById("contactForm");
+const confirm = document.getElementById("confirmation");
 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  confirm.classList.remove("hidden");
+  form.reset();
+});
